3a608d474107f1822f0a93b0260fa2e5711ee17a
ristukaJJang <ghwjd5585@gmail.com>
2025-01-04 14:09:17 +0900

    finish to navigate pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes, useNavigate, useSearchParams } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { ACCESS_TOKEN, GEN_DISC_PATH, LOGIN_ABSOLUTE_PATH, LOGIN_PATH, MAIN_ABSOLUTE_PATH, MAIN_PATH, MY_PATH, NOTICE, OTHERS_PATH, ROOT_PATH, RT_DISC_PATH, SCHEDULE, SNS_SUCCESS_PATH } from './constants';
+import MainLayout from './layouts/MainLayout';
+import GerneralDiscuss from './view/General_Discuss';
+import RTDiscuss from './view/RT_Discuss';
+import Notice from './view/Notice';
+import Schedule from './view/Schedule';
+import Main from './view/Main';
+import Mypage from './view/Mypage';
+import Login from './view/Auth/Login';
 
-function App() {
+// component: root path 컴포넌트 //
+function Index() {
+
+  // state: 쿠키 상태 //
+  const [cookies] = useCookies();
+
+  // function: 네비게이터 함수 //
+  const navigator = useNavigate();
+
+  // effect: 마운트 시 경로 이동 effect //
+  useEffect(()=> {
+    if(cookies[ACCESS_TOKEN]) navigator(MAIN_ABSOLUTE_PATH);
+    else navigator(LOGIN_ABSOLUTE_PATH);
+  }, []);
+
+  // render: root path 컴포넌트 렌더링 //
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
-export default App;
+
+// component: sns success 컴포넌트 //
+function SnsSuccess() {
+
+  // state: query parameter 상태 //
+  const [queryParam] = useSearchParams();
+  const accessToken = queryParam.get('accessToken');
+  const expiration = queryParam.get('expiration');
+
+  // state: cookie 상태 //
+  const [cookies, setCookie] = useCookies();
+
+  // function: 네비게이터 함수 //
+  const navigator = useNavigate();
+
+  // effect: sns success 컴포넌트 로드시 accessToken과 expiration을 확인하여 로그인 처리하는 함수 //
+  useEffect(()=> {
+    if(accessToken && expiration) {
+      const expires = new Date(Date.now() + (Number(expiration) * 1000));
+      setCookie(ACCESS_TOKEN, accessToken, {path: ROOT_PATH, expires});
+
+      navigator(MAIN_ABSOLUTE_PATH);
+    }else {
+      navigator(LOGIN_ABSOLUTE_PATH);
+    }
+  }, []);
+
+  // render: sns success 컴포넌트 렌더링 //
+  return <></>;
+
+}
+
+// component: 도란도란 컴포넌트 //
+export default function DoranDoran() {
+  
+    // render: 메인 화면 렌더링 //
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Index />} />
+          <Route path={LOGIN_PATH} element={<Login />} />
+
+          <Route path={MAIN_PATH} element={<MainLayout/>} >
+            <Route index element={<Main />} />
+          </Route>
+
+          <Route path={GEN_DISC_PATH} element={<MainLayout/>}>
+            <Route index element={<GerneralDiscuss />} />
+          </Route>
+
+          <Route path={RT_DISC_PATH} element={<MainLayout/>}>
+            <Route index element={<RTDiscuss />} />
+          </Route>
+
+          <Route path={NOTICE} element={<MainLayout/>} >
+            <Route index element={<Notice />} />
+          </Route>
+
+          <Route path={SCHEDULE} element={<MainLayout/>} >
+            <Route index element={<Schedule />} />
+          </Route>
+
+          <Route path={MY_PATH} element={<MainLayout />}  >
+            <Route index element={<Mypage />} />
+          </Route>
+
+          <Route path={SNS_SUCCESS_PATH} element={<SnsSuccess/>} />
+          <Route path={OTHERS_PATH} element={<Index />}/>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
---
+++ src/constants/index.tsx
@@ -0,0 +1,36 @@
+// variable: 상대 경로 상수 //
+export const ROOT_PATH = '/';
+
+export const LOGIN_PATH = '/login';
+
+export const MAIN_PATH = '/main';
+
+export const GEN_DISC_PATH = '/gen_disc';
+export const RT_DISC_PATH = '/rt_disc';
+export const NOTICE = '/notice';
+export const SCHEDULE = '/schedule';
+
+export const MY_PATH = '/mypage';
+
+export const SNS_SUCCESS_PATH = '/sns-success';
+export const OTHERS_PATH = '*';
+
+
+// variable: 절대 경로 상수 //
+export const ROOT_ABSOLUTE_PATH = ROOT_PATH;
+
+export const LOGIN_ABSOLUTE_PATH = LOGIN_PATH;
+
+export const MAIN_ABSOLUTE_PATH = MAIN_PATH;
+
+export const GEN_DISC_ABSOLUTE_PATH = GEN_DISC_PATH;
+export const RT_DISC_ABSOLUTE_PATH = RT_DISC_PATH;
+export const NOTICE_ABSOLUTE_PATH = NOTICE;
+export const SCHEDULE_ABSOLUTE_PATH = SCHEDULE;
+
+export const MY_ABSOLUTE_PATH = MY_PATH;
+
+export const OTHERS_ABSOLUTE_PATH = OTHERS_PATH;
+
+// variable: HTTP BEARER TOKEN COOKIE NAME //
+export const ACCESS_TOKEN = 'accessToken';
