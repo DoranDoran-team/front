24df72c9e46271cc1e9d40ba526145361263ccf3
Song-Tae-Hwi <thdxognl4132@gmail.com>
2025-01-16 15:08:01 +0900

    merge

--- src/view/Mypage/Admin/index.tsx
+++ src/view/Mypage/Admin/index.tsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import './style.css';
 import { useNavigate } from 'react-router-dom';
 import { ADMIN_ABSOLUTE_ACCUSE_PATH, ADMIN_ABSOLUTE_MILEAGE_PATH, ADMIN_ACCUSE_PATH, ADMIN_MILEAGE_PATH } from '../../../constants';
@@ -11,9 +11,9 @@ export default function Admin() {
     const [state] = useState<boolean>(false);
     const [editbutton, setEditButton] = useState<boolean>(false);
     const [toggleDown, setToggleDown] = useState<boolean>(false)
-        const [sortingState, setSortingState] = useState({
-            selected: '정렬순'
-        })
+    const [sortingState, setSortingState] = useState({
+        selected: '정렬순'
+    })
 
     // function: 네비게이터 함수 처리 //
     const navigator = useNavigate();
@@ -35,10 +35,10 @@ export default function Admin() {
         setSortingState((prevState) => ({
             ...prevState
         }));
-        
+
     };
     // event handler: 정렬 메뉴 아이템 클릭 이벤트 처리 함수 //
-    const onSortOptionClickHandler = (option:string) => {
+    const onSortOptionClickHandler = (option: string) => {
         setSortingState({
             selected: option,
         })
@@ -55,10 +55,10 @@ export default function Admin() {
     return (
         <div className="mypage-wrapper">
             <div className="admin-side-wrapper">
-                            <AdminSideBar/>
-                        </div>
+                <AdminSideBar />
+            </div>
             <div className="mypage-main-wrapper">
-            <div className="top-icon-box">
+                <div className="top-icon-box">
                     <div className="top-icons">
                         <div className="top-icon-menu" onClick={onMenuButtonHandler}>
                             {menu && (<div className='menu-list' >
@@ -80,8 +80,8 @@ export default function Admin() {
                     <div className="mypage-discussion-room">실시간 토론방 현황</div>
                     <div className="discussion-state-box" onClick={onSortingButtonHandler}>{sortingState.selected}
                         {toggleDown && <div className="state-type-box" >
-                            <div className="state-type" onClick={()=>onSortOptionClickHandler('진행중')}>진행중</div>
-                            <div className="state-type" onClick={()=>onSortOptionClickHandler('마감')}>마감</div>
+                            <div className="state-type" onClick={() => onSortOptionClickHandler('진행중')}>진행중</div>
+                            <div className="state-type" onClick={() => onSortOptionClickHandler('마감')}>마감</div>
                         </div>}
                     </div>
                 </div>
@@ -117,40 +117,40 @@ export default function Admin() {
                             </div>
                         </div>
                     </div>
-                </div>:
-                <div className="discussion-room-list">
-                    <div className="discussion-image"></div>
-                    <div className="discussion-info">
-                        <div className="discussion-title-box">
-                            <div className="discussion-title">대마초 합법화</div>
-                            <div className="discussion-icon-box">
-                                <div className="discussion-icon" onClick={onPostMenuButtonHandler}></div>
-                                {editbutton && <div className="discussion-edit-box">
-                                    <div className="edit-item">수정</div>
-                                    <div className="edit-item">삭제</div>
-                                </div>}
+                </div> :
+                    <div className="discussion-room-list">
+                        <div className="discussion-image"></div>
+                        <div className="discussion-info">
+                            <div className="discussion-title-box">
+                                <div className="discussion-title">대마초 합법화</div>
+                                <div className="discussion-icon-box">
+                                    <div className="discussion-icon" onClick={onPostMenuButtonHandler}></div>
+                                    {editbutton && <div className="discussion-edit-box">
+                                        <div className="edit-item">수정</div>
+                                        <div className="edit-item">삭제</div>
+                                    </div>}
+                                </div>
+                            </div>
+                            <div className="discussion-contents">범죄 감소와 세수 증대 효과가 있다. vs 건강 문제와 사회적 부작용이 우려된다.</div>
+                            <div className="discussion-bottom">
+                                <div className="discussion-bottom-box">
+                                    <div className="discussion-created">20204.12.30 16:30</div>
+                                    <div className="discussion-fixed">(수정됨)</div>
+                                    {!state ? <div className="discussion-state-box continue">
+                                        <div className="discussion-state ">진행중</div>
+                                    </div> :
+                                        <div className="discussion-state-box end">
+                                            <div className="discussion-state ">마감</div>
+                                        </div>
+                                    }
+                                </div>
+                                <div className="discussion-icons">
+                                    <div className="discussion-participants-icon"></div>
+                                    <div className="discussion-comment">6</div>
+                                </div>
                             </div>
                         </div>
-                        <div className="discussion-contents">범죄 감소와 세수 증대 효과가 있다. vs 건강 문제와 사회적 부작용이 우려된다.</div>
-                        <div className="discussion-bottom">
-                            <div className="discussion-bottom-box">
-                                <div className="discussion-created">20204.12.30 16:30</div>
-                                <div className="discussion-fixed">(수정됨)</div>
-                                {!state ? <div className="discussion-state-box continue">
-                                    <div className="discussion-state ">진행중</div>
-                                </div> :
-                                    <div className="discussion-state-box end">
-                                        <div className="discussion-state ">마감</div>
-                                    </div>
-                                }
-                            </div>
-                            <div className="discussion-icons">
-                                <div className="discussion-participants-icon"></div>
-                                <div className="discussion-comment">6</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>}
+                    </div>}
             </div>
         </div>
     )
