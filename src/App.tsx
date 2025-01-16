760c48f8cfef3c19157884f51ffd5bc576cfbbd7
Song-Tae-Hwi <thdxognl4132@gmail.com>
2025-01-16 13:59:51 +0900

    main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes, useNavigate, useSearchParams } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import { ACCESS_TOKEN, GEN_DISC_DETAIL_ABSOLUTE_PATH, GEN_DISC_PATH, GEN_DISC_WRITE_ABSOLUTE_PATH, LOGIN_ABSOLUTE_PATH, LOGIN_PATH, MAIN_ABSOLUTE_PATH, MAIN_PATH, MY_PATH, NOTICE, OTHERS_PATH, ROOT_PATH, RT_DISC_PATH, SCHEDULE, SNS_SUCCESS_PATH,CHANGE_PW, FIND_ID, FIND_ID_RESULT, FIND_PW, SIGN_UP, ADMIN_PATH, MY_UPDATE_PATH } from './constants';
+import { ACCESS_TOKEN, GEN_DISC_DETAIL_ABSOLUTE_PATH, GEN_DISC_PATH, GEN_DISC_WRITE_ABSOLUTE_PATH, LOGIN_ABSOLUTE_PATH, LOGIN_PATH, MAIN_ABSOLUTE_PATH, MAIN_PATH, MY_PATH, NOTICE, OTHERS_PATH, ROOT_PATH, RT_DISC_PATH, SCHEDULE, SNS_SUCCESS_PATH, CHANGE_PW, FIND_ID, FIND_ID_RESULT, FIND_PW, SIGN_UP, ADMIN_PATH, MY_UPDATE_PATH, NOTICE_WRITE } from './constants';
 import MainLayout from './layouts/MainLayout';
 import GD from './view/General_Discuss';
 import RTDiscuss from './view/RT_Discuss';
@@ -21,6 +21,7 @@ import ChangePw from './view/Auth/Change-pw';
 
 import Update from './view/Mypage/Update';
 import Admin from './view/Mypage/Admin';
+import NoticeWrite from './view/Notice/Write';
 
 // component: root path 컴포넌트 //
 function Index() {
@@ -77,53 +78,54 @@ function SnsSuccess() {
 
 // component: 도란도란 컴포넌트 //
 export default function DoranDoran() {
-  
+
   const roomId = "123"; // 일단 roomId 하드코딩
-  
-    // render: 메인 화면 렌더링 //
-    return (
-        <Routes>
-          <Route index element={<Index />} />
-          <Route path={LOGIN_PATH} element={<Login />}></Route>
-          <Route path={FIND_ID} element={<FindId/>}/>
-          <Route path={FIND_ID_RESULT} element={<FindIdResult/>} />
-          <Route path={FIND_PW} element={<FindPw/>}></Route>
-          <Route path={CHANGE_PW} element={<ChangePw/>} />
-          <Route path={SIGN_UP} element={<SignUp/>}></Route>
 
-          <Route path={MAIN_PATH} element={<MainLayout/>} >
-            <Route index element={<Main />} />
-          </Route>
+  // render: 메인 화면 렌더링 //
+  return (
+    <Routes>
+      <Route index element={<Index />} />
+      <Route path={LOGIN_PATH} element={<Login />}></Route>
+      <Route path={FIND_ID} element={<FindId />} />
+      <Route path={FIND_ID_RESULT} element={<FindIdResult />} />
+      <Route path={FIND_PW} element={<FindPw />}></Route>
+      <Route path={CHANGE_PW} element={<ChangePw />} />
+      <Route path={SIGN_UP} element={<SignUp />}></Route>
 
-          <Route path={GEN_DISC_PATH} element={<MainLayout/>}>
-            <Route index element={<GD />} />
-            <Route path={GEN_DISC_DETAIL_ABSOLUTE_PATH(roomId)} element={<GDDetail />} />
-            <Route path={GEN_DISC_WRITE_ABSOLUTE_PATH} element={<GDWrite />} />
-          </Route>
+      <Route path={MAIN_PATH} element={<MainLayout />} >
+        <Route index element={<Main />} />
+      </Route>
 
-          <Route path={RT_DISC_PATH} element={<MainLayout/>}>
-            <Route index element={<RTDiscuss />} />
-          </Route>
+      <Route path={GEN_DISC_PATH} element={<MainLayout />}>
+        <Route index element={<GD />} />
+        <Route path={GEN_DISC_DETAIL_ABSOLUTE_PATH(roomId)} element={<GDDetail />} />
+        <Route path={GEN_DISC_WRITE_ABSOLUTE_PATH} element={<GDWrite />} />
+      </Route>
 
-          <Route path={NOTICE} element={<MainLayout/>} >
-            <Route index element={<Notice />} />
-          </Route>
+      <Route path={RT_DISC_PATH} element={<MainLayout />}>
+        <Route index element={<RTDiscuss />} />
+      </Route>
 
-          <Route path={SCHEDULE} element={<MainLayout/>} >
-            <Route index element={<Schedule />} />
-          </Route>
+      <Route path={NOTICE} element={<MainLayout />} >
+        <Route index element={<Notice />} />
+        <Route path={NOTICE_WRITE} element={<NoticeWrite />} />
+      </Route>
 
-          <Route path={MY_PATH} element={<MainLayout />}  >
-            <Route index element={<Mypage />} />
-            <Route path={MY_UPDATE_PATH(':userId')} element={<Update />}/>
-          </Route>
+      <Route path={SCHEDULE} element={<MainLayout />} >
+        <Route index element={<Schedule />} />
+      </Route>
 
-          <Route path={ADMIN_PATH} element={<MainLayout />}>
-            <Route index element={<Admin />}/>
-          </Route>
+      <Route path={MY_PATH} element={<MainLayout />}  >
+        <Route index element={<Mypage />} />
+        <Route path={MY_UPDATE_PATH(':userId')} element={<Update />} />
+      </Route>
 
-          <Route path={SNS_SUCCESS_PATH} element={<SnsSuccess/>} />
-          <Route path={OTHERS_PATH} element={<Index />}/>
-        </Routes>
-    );
-  }
+      <Route path={ADMIN_PATH} element={<MainLayout />}>
+        <Route index element={<Admin />} />
+      </Route>
+
+      <Route path={SNS_SUCCESS_PATH} element={<SnsSuccess />} />
+      <Route path={OTHERS_PATH} element={<Index />} />
+    </Routes>
+  );
+}
